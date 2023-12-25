099a3baa9ba75ec8acb6ab98eac9bab35c2a5e8f
Sharns <oskarsharns@gmail.com>
2023-12-25 02:30:55 +0200

    Added all CRUD operation for Event controller  + service + interface

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -17,11 +17,94 @@ namespace EventFinder.Controllers
             _eventService = eventService;
         }
 
-        [HttpPost]
-        public IActionResult CreateEvent([FromBody] Event newEvent)
+        [HttpPost("CreateEvent")]
+        public async Task<ActionResult<Event>> CreateEvent([FromBody] Event newEvent)
         {
-            var result = _eventService.CreateEvent(newEvent);
-            return Ok(result);
+            try
+            {
+                var result = await _eventService.CreateEvent(newEvent);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error creating event: {ex.Message}");
+            }
+        }
+
+        [HttpGet("GetEventById/{id}")]
+        public async Task<ActionResult<Event>> GetEventById(Guid id)
+        {
+            try
+            {
+                var result = await _eventService.GetEventById(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error getting event: {ex.Message}");
+            }
+        }
+
+        [HttpGet("GetAllEvents")]
+        public async Task<ActionResult<List<Event>>> GetAllEvents()
+        {
+            try
+            {
+                var result = await _eventService.GetAllEvents();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error getting all events, error: {ex.Message}");
+            }
+        }
+
+        [HttpPut("UpdateEvent")]
+        public async Task<ActionResult<Event>> UpdateEvent(Guid id, Event eventToUpdate)
+        {
+            try
+            {
+                var result = await _eventService.UpdateEvent(id, eventToUpdate);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(eventToUpdate);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error updating event: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("DeleteEvent")]
+        public async Task<ActionResult<Event>> DeleteEvent(Guid id)
+        {
+            try
+            {
+                var result = await _eventService.DeleteEvent(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error deleting event: {ex.Message}");
+            }
         }
     }
 }
--- Services/Interfaces/IEventService.cs
+++ Services/Interfaces/IEventService.cs
@@ -4,6 +4,10 @@ namespace EventFinder.Services.Interfaces
 {
     public interface IEventService
     {
-        public Event CreateEvent(Event newEvent);
+        public Task<Event> CreateEvent(Event newEvent);
+        public Task<Event> GetEventById(Guid id);
+        public Task<List<Event>> GetAllEvents();
+        public Task<Event> UpdateEvent(Guid id, Event evenToUpdate);
+        public Task<Event> DeleteEvent(Guid id);
     }
 }
